2cc3fbf00198eaf9fb6217b1bfb56e1b05f6b539
JafarKMarouf <jafar.k.marouf@gmail.com>
2024-08-26 03:32:52 -0400

    implement comment controller functions

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,17 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreCommentRequest;
+use App\Http\Requests\UpdateCommentRequest;
 use App\Models\Comment;
+use App\Models\Post;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CommentController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index($post_id)
     {
-        //
+        try {
+            $post = Post::find($post_id);
+            if (!$post) {
+                return response()->json([
+                    'status' => 'failed',
+                    'message' => 'post not found'
+                ], 404);
+            }
+            $comments = $post->comments()->with('user:id,name,image')->get();
+            return response()->json([
+                'status' => 'success',
+                'data' => $comments,
+                'message' => 'Comments fetched Successfully'
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => 'faild',
+                'message' => $e->getMessage()
+            ], 500);
+        }
     }
 
     /**
@@ -26,17 +50,64 @@ class CommentController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreCommentRequest $request, $post_id)
     {
-        //
+        try {
+            $post = Post::find($post_id);
+            if (!$post) {
+                return response()->json([
+                    'status' => 'failed',
+                    'message' => 'post not found'
+                ], 404);
+            }
+            $user_id = Auth::user()->id;
+            $comment = Comment::create([
+                'user_id' => $user_id,
+                'post_id' => $post_id,
+                'comment_body' => $request->comment_body,
+            ]);
+            return response()->json([
+                'status' => 'success',
+                'data' => $comment,
+                'message' => 'Comments fetched Successfully'
+
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => 'faild',
+                'message' => $e->getMessage()
+            ], 500);
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Comment $comment)
+    public function show($comment_id)
     {
-        //
+        try {
+            $comment = Comment::find($comment_id);
+            if (!$comment) {
+                return response()->json([
+                    'status' => 'failed',
+                    'message' => 'comment not found'
+                ], 404);
+            }
+            $comment = Comment::query()->where('id', $comment_id)
+                ->with('user:id,name,image')
+                ->get(['id', 'user_id', 'comment_body', 'created_at']);
+            return response()->json([
+                'status' => 'success',
+                'data' => $comment,
+                'message' => 'Comment fetched Successfully'
+
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => 'faild',
+                'message' => $e->getMessage()
+            ], 500);
+        }
     }
 
     /**
@@ -50,16 +121,70 @@ class CommentController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Comment $comment)
+    public function update(UpdateCommentRequest $request, $comment_id)
     {
-        //
+        try {
+            $comment = Comment::find($comment_id);
+            if (!$comment) {
+                return response()->json([
+                    'status' => 'failed',
+                    'message' => 'comment not found'
+                ], 404);
+            }
+            $user_id = Auth::user()->id;
+            if ($comment->user_id != $user_id) {
+                return response()->json([
+                    'status' => 'failed',
+                    'message' => 'Permission denied.'
+                ], 403);
+            }
+            $comment->update([
+                'comment_body' => $request->comment_body ?? $comment->comment_body,
+            ]);
+            return response()->json([
+                'status' => 'success',
+                'data' => $comment,
+                'message' => 'Comments updated Successfully!',
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => 'faild',
+                'message' => $e->getMessage()
+            ], 500);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Comment $comment)
+    public function destroy($comment_id)
     {
-        //
+        try {
+            $comment = Comment::find($comment_id);
+            if (!$comment) {
+                return response()->json([
+                    'status' => 'failed',
+                    'message' => 'comment not found'
+                ], 404);
+            }
+            $user_id = Auth::user()->id;
+            if ($comment->user_id != $user_id) {
+                return response()->json([
+                    'status' => 'failed',
+                    'message' => 'Permission denied.'
+                ], 403);
+            }
+            $comment->delete();
+            return response()->json([
+                'status' => 'success',
+                'data' => [],
+                'message' => 'Comments deleted Successfully!',
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => 'faild',
+                'message' => $e->getMessage()
+            ], 500);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthUserController;
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
@@ -13,4 +14,12 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('logout', [AuthUserController::class, 'logout'])->name('logout');
 
     Route::resource('post', PostController::class);
+
+    Route::prefix('post')->group(function () {
+        Route::get('{post_id}/comment', [CommentController::class, 'index']);
+        Route::post('{post_id}/comment', [CommentController::class, 'store']);
+    });
+    Route::get('comment/{comment_id}', [CommentController::class, 'show']);
+    Route::put('comment/{comment_id}', [CommentController::class, 'update']);
+    Route::delete('comment/{comment_id}', [CommentController::class, 'destroy']);
 });
