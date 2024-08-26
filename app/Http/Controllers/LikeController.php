bf89dbf96ab20b1f9fd4413db49bdee73be1b71a
JafarKMarouf <jafar.k.marouf@gmail.com>
2024-08-26 03:48:48 -0400

    implement like controller

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -3,63 +3,78 @@
 namespace App\Http\Controllers;
 
 use App\Models\Like;
+use App\Models\Post;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LikeController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
-    {
-        //
-    }
+    public function index() {}
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
-    {
-        //
-    }
+    public function create() {}
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function likeOrUnLike($post_id)
     {
-        //
+        try {
+            $post = Post::find($post_id);
+            if (!$post) {
+                return response()->json([
+                    'status' => 'failed',
+                    'message' => 'Post Not Found!',
+                ], 404);
+            }
+            $user_id = Auth::user()->id;
+            $like = $post->likes()->where('user_id', $user_id)->first();
+            if (!$like) {
+                Like::create([
+                    'post_id' => $post_id,
+                    'user_id' => $user_id
+                ]);
+                return response()->json([
+                    'status' => 'success',
+                    'message' => 'Liked'
+                ], 200);
+            }
+            $like->delete();
+            return response()->json([
+                'status' => 'success',
+                'message' => 'unliked'
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => 'faild',
+                'message' => $e->getMessage()
+            ], 500);
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Like $like)
-    {
-        //
-    }
+    public function show(Like $like) {}
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Like $like)
-    {
-        //
-    }
+    public function edit(Like $like) {}
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Like $like)
-    {
-        //
-    }
+    public function update(Request $request, Like $like) {}
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Like $like)
-    {
-        //
-    }
+    public function destroy(Like $like) {}
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthUserController;
 use App\Http\Controllers\CommentController;
+use App\Http\Controllers\LikeController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
@@ -22,4 +23,6 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('comment/{comment_id}', [CommentController::class, 'show']);
     Route::put('comment/{comment_id}', [CommentController::class, 'update']);
     Route::delete('comment/{comment_id}', [CommentController::class, 'destroy']);
+
+    Route::post('post/{post_id}/likeOrUnLike', [LikeController::class, 'likeOrUnLike']);
 });
